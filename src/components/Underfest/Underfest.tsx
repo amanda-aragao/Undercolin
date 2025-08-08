32f59f7efa243dcdc02a457c64af29e69d75e08a
Amanda Aragão <amandaaragao72@gmail.com>
2025-08-08 14:43:02 -0300

    ajustando caminhos das imagens da parte da underfest para aparecer corretamente no vercel

--- src/components/Underfest/Underfest.tsx
+++ src/components/Underfest/Underfest.tsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
 import './Underfest.css'
 import { MaterialUnderfest } from '../CarouselUnderfest/MaterialUnderfest.tsx'
+import aphoyce from '../../assets/bandasUnderfest/Aphoyce.png';
+import cannonOfhate from '../../assets/bandasUnderfest/COH.png';
+import triunfe from '../../assets/bandasUnderfest/triunfe.png';
+import undercolin from '../../assets/bandasUnderfest/undercolin.png';
+import NEET from '../../assets/bandasUnderfest/NEET.png';
+import rabujentus from '../../assets/bandasUnderfest/rabujentus.png';
+import DDT from '../../assets/bandasUnderfest/DDT.png';
+import EP from '../../assets/bandasUnderfest/EP.png';
 
 export function Underfest() {
 
@@ -16,31 +24,31 @@ export function Underfest() {
           <p className='subTitle'>
             O UNDERFEST É UM FESTIVAL ORGANIZADO ANUALMENTE PELA BANDA UNDERCOLIN DE FORMA 100% INDEPENDENTE. <br />
             O EVENTO REÚNE BANDAS E ARTISTAS DA CENA ALTERNATIVA, PROPORCIONANDO UM ESPAÇO PARA NOVAS E JÁ CONSOLIDADAS BANDAS SE APRESENTAREM. </p>
-          <h3 className='titleBands'>BANDAS QUE PARTICIPANTES</h3>
+          <h3 className='titleBands'>BANDAS PARTICIPANTES</h3>
           <div className='bands'>
             <Link to='https://www.instagram.com/aphoyceh2core/' target='_blanck' >
-              <img src='../../src/assets/bandasUnderfest/Aphoyce.png' alt='bandaAphoyce' width={250} />
+              <img src={aphoyce} alt="logo-aphoyce" />
             </Link>
             <Link to='https://www.instagram.com/cannonofhate/' target='_blanck'>
-              <img src='../../src/assets/bandasUnderfest/COH.png' alt='bandaCannonOfHate' width={450} />
+              <img src={cannonOfhate} alt='bandaCannonOfHate' />
             </Link>
-            <Link to='https://www.instagram.com/triunfeoficial/' target='_blanck'>
-              <img src='../../assets/bandasUnderfest/triunfe.png' alt='bandaTriunfe' width={450} />
+            <Link to='https://www.instagram.com/triunfeoficial/' className='triunfe' target='_blanck'>
+              <img src={triunfe} alt='bandaTriunfe' />
             </Link>
-            <Link to='https://www.instagram.com/undercolin_/' target='_blanck'>
-              <img src='../../assets/bandasUnderfest/undercolin.png' alt='bandaUnderColin' width={190} />
-            </Link>
-            <Link to='https://www.instagram.com/undercolin_/' target='_blanck'>
-              <img src='../../assets/bandasUnderfest/NEET.png' alt='bandaUnderColin' width={190} />
+            <Link to='https://www.instagram.com/undercolin/' target='_blanck' className='bandaUndercolin'>
+              <img src={undercolin} alt='bandaUndercolin' />
             </Link>
             <Link to='https://www.instagram.com/undercolin_/' target='_blanck'>
-              <img src='../../assets/bandasUnderfest/rabujentus.png' alt='bandaUnderColin' width={300} />
+              <img src={NEET} alt='NEET' />
             </Link>
             <Link to='https://www.instagram.com/undercolin_/' target='_blanck'>
-              <img src='../../assets/bandasUnderfest/DDT.png' alt='bandaUnderColin' width={350} />
+              <img src={rabujentus} alt='Rabujentus' />
             </Link>
             <Link to='https://www.instagram.com/undercolin_/' target='_blanck'>
-              <img src='../../assets/bandasUnderfest/EP.png' alt='bandaUnderColin' width={350} />
+              <img src={DDT} alt='DDT' />
+            </Link>
+            <Link to='https://www.instagram.com/undercolin_/' target='_blanck'>
+              <img src={EP} alt='EP' />
             </Link>
           </div>
 
